eda97d7c41e3cc9eaca57fd6fc27c97d772fa0a8
Jjjulietta <jjjuliettai@gmail.com>
2023-04-15 00:02:49 +0300

    feat: add solutions

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,12 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class ListNode {
-  constructor(value){
-    this.value = value;
-    this.next = null
-  }
-}
+
 
 class Queue {
   constructor(){
@@ -27,21 +22,12 @@ class Queue {
   }
   getUnderlyingList() {
     if(!this.head){return null} else { 
-      let obj = {}
-      let current = this.head;
-      while(current.next) {
-        obj[value] = current.value;
-        obj[next] = current.next.value;
-       current = current.next
-        
-      }
-      return obj;
+           return this.head.next;
     }
     // remove line with error and write your code here
   }
 
   enqueue(value) {
-    console.debug(value)
     if(!this.head) {
       this.head = new ListNode(value)
     }
@@ -63,7 +49,7 @@ class Queue {
       
     }  else { this.head = null; }
     this.length --;
-   
+   return current.next.value
   
     
     
